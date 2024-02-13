2750da50f6e3c3f71ceafdc19444ed29d51960c3
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-02-13 09:16:28 +0700

    Install babel-plugin-module-resolver

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,3 @@
-module.exports = function override(config, env) {
-    //do stuff with the webpack config...
-    return config;
-};
+const { override, useBabelRc } = require("customize-cra");
+
+module.exports = override(useBabelRc());
