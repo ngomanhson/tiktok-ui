453d1801ffdd7244b3433e66f239817c56601cbc
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-02-13 22:01:44 +0700

    Build UI Header #3

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import Tippy from "@tippyjs/react/headless";
 
+import Button from "~/components/Button";
 import { Wrapper as PopperWrapper } from "~/components/Popper";
 import styles from "./Header.module.scss";
 import images from "~/assets/images";
@@ -54,7 +55,10 @@ function Header() {
                     </div>
                 </Tippy>
 
-                <div className={cx("actions")}></div>
+                <div className={cx("actions")}>
+                    <Button text>Upload</Button>
+                    <Button primary>Log in</Button>
+                </div>
             </div>
         </header>
     );
