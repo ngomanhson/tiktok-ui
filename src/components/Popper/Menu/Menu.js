0d2fdbfcb3b18fbb09077056a92d22faf7f29d17
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-02-15 22:29:07 +0700

    Fix Header #3

--- src/components/Popper/Menu/Menu.js
+++ src/components/Popper/Menu/Menu.js
@@ -34,31 +34,26 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
             );
         });
     };
+    const handleBack = () => {
+        setHistory((prev) => prev.slice(0, prev.length - 1));
+    };
+
+    const renderResult = (attrs) => (
+        <div className={cx("menu-list")} tabIndex="-1" {...attrs}>
+            <PopperWrapper className={cx("menu-popper")}>
+                {history.length > 1 && <Header title={current.title} onBack={handleBack} />}
+                <div className={cx("menu-body")}>{renderItems()}</div>
+            </PopperWrapper>
+        </div>
+    );
+
+    // Reset to first page
+    const handleReset = () => {
+        setHistory((prev) => prev.slice(0, 1));
+    };
 
     return (
-        <Tippy
-            interactive
-            delay={[0, 800]}
-            offset={[12, 8]}
-            hideOnClick={hideOnClick}
-            placement="bottom-end"
-            render={(attrs) => (
-                <div className={cx("menu-list")} tabIndex="-1" {...attrs}>
-                    <PopperWrapper className={cx("menu-popper")}>
-                        {history.length > 1 && (
-                            <Header
-                                title={current.title}
-                                onBack={() => {
-                                    setHistory((prev) => prev.slice(0, prev.length - 1));
-                                }}
-                            />
-                        )}
-                        <div className={cx("menu-body")}>{renderItems()}</div>
-                    </PopperWrapper>
-                </div>
-            )}
-            onHidden={() => setHistory((prev) => prev.slice(0, 1))}
-        >
+        <Tippy interactive delay={[0, 800]} offset={[12, 8]} hideOnClick={hideOnClick} placement="bottom-end" render={renderResult} onHidden={handleReset}>
             {children}
         </Tippy>
     );
--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -17,22 +17,22 @@ const cx = classNames.bind(styles);
 function Search() {
     const [searchValue, setSearchValue] = useState("");
     const [searchResult, setSearchResult] = useState([]);
-    const [showResult, setShowResult] = useState(true);
+    const [showResult, setShowResult] = useState(false);
     const [loading, setLoading] = useState(false);
 
-    const debounced = useDebounce(searchValue, 500);
+    const debouncedValue = useDebounce(searchValue, 500);
 
     const inputRef = useRef();
 
     useEffect(() => {
-        if (!debounced.trim()) {
+        if (!debouncedValue.trim()) {
             setSearchResult([]);
             return;
         }
 
         const fetchApi = async () => {
             setLoading(true);
-            const result = await searchServices.search(debounced);
+            const result = await searchServices.search(debouncedValue);
 
             setSearchResult(result);
 
@@ -40,7 +40,7 @@ function Search() {
         };
 
         fetchApi();
-    }, [debounced]);
+    }, [debouncedValue]);
 
     const handleClear = () => {
         setSearchValue("");
